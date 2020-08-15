6394b43dc791b25711a4d9a2231716f1b50886d9
Ibán López <ibanlopezd@gmail.com>
2020-08-15 15:35:59 +0200

    Updates

---
+++ precache-manifest.1d96dc21e1b67afc61418193d940c738.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd0615160b54000560c546092863f53",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "7d229f93979a83ca7515",
+    "url": "/calculator/static/css/main.4594faab.chunk.css"
+  },
+  {
+    "revision": "a96034716a04440544c9",
+    "url": "/calculator/static/js/2.7d54b4ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.7d54b4ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebec1cc99754d33b99f2",
+    "url": "/calculator/static/js/3.6a46de29.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/calculator/static/js/3.6a46de29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d229f93979a83ca7515",
+    "url": "/calculator/static/js/main.0234737d.chunk.js"
+  },
+  {
+    "revision": "9e522c91ea24db507b69",
+    "url": "/calculator/static/js/runtime-main.f423230b.js"
+  },
+  {
+    "revision": "40c685dd49204b0e922cab54004d3aa2",
+    "url": "/calculator/static/media/paddle.40c685dd.mp3"
+  }
+]);
